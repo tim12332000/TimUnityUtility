1d3a7178754911fd5901d2fe92ecae48867535df
timtim <tim12332000@gmail.com>
2018-11-02 10:33:09 +0800

    add coroutineTasks

---
+++ Assets/Scripts/CoroutineTasks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tim.Utility
+{
+	public class CoroutineTasks
+	{
+		private Queue<IEnumerator> _tasks = new Queue<IEnumerator>();
+		private bool _isDirty;
+
+		public void Enqueue(IEnumerator task)
+		{
+			_tasks.Enqueue(task);
+		}
+
+		public IEnumerator Play(Action onFinish = null)
+		{
+			if (_isDirty)
+			{
+				Debug.LogWarningFormat("[CoroutineTasks][Play] This Queue is already playing.");
+				yield break;
+			}
+
+			_isDirty = true;
+			while (_tasks.Count > 0)
+			{
+				yield return _tasks.Dequeue();
+			}
+
+			_isDirty = false;
+			if (onFinish != null)
+				onFinish();
+		}
+	}
+}
