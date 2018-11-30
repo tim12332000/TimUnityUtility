5b7b92cfea64ad88b234d7b704f7057424b33713
timtim <tim12332000@gmail.com>
2018-11-30 18:36:10 +0800

    Add CheckerSequence Add Nsubsititute
    Add CheckerSequence Add Nsubsititute

---
+++ Assets/ChceckerSequence/CheckerSequence.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tim.Utility.CheckerSequence
+{
+	/// <summary>
+	/// Unity Can't Serialize Interface ,So Create this class for editor.
+	/// </summary>
+	public abstract class CheckerMono : MonoBehaviour, IChecker
+	{
+		public abstract void Check(Action<bool> isNeedPlay);
+		public abstract void Play(Action done);
+	}
+
+	public interface IChecker
+	{
+		void Play(Action done);
+		void Check(Action<bool> isNeedPlay);
+	}
+
+	public class CheckerSequence
+	{
+		public Action OnPlayBegin = delegate { };
+		public Action OnPlayAllDone = delegate { };
+		public event Action OnCheckStart = delegate { };
+		public event Action OnCheckEnd = delegate { };
+
+		private List<IChecker> _checkerSequence = new List<IChecker>();
+		private int _index = 0;
+		private int _playedCount = 0;
+
+		public int PlayedCount
+		{
+			get
+			{
+				return _playedCount;
+			}
+		}
+
+		public void Insert(IChecker ic, int order)
+		{
+			_checkerSequence.Insert(order, ic);
+		}
+
+		/// <summary>
+		/// Join the last one
+		/// </summary>
+		/// <param name="c"></param>
+		public void Add(IChecker c)
+		{
+			_checkerSequence.Add(c);
+		}
+
+		public void Start()
+		{
+			if (_index > 0)
+			{
+				Debug.LogError("[CheckerSequence][Start] sequence is Playing , Finish Not Yet!");
+				return;
+			}
+
+			_playedCount = 0;
+			OnPlayBegin.Invoke();
+			CheckAndPlay();
+		}
+
+		private void CheckAndPlay()
+		{
+			if (_index < _checkerSequence.Count)
+			{
+				IChecker unit = _checkerSequence[_index];
+				_index++;
+
+				string className = unit.GetType().Name;
+				Debug.LogFormat("[CheckerSequence][CheckAndPlay] className Checking {0} , _index {1}", className, _index);
+
+				OnCheckStart.Invoke();
+				unit.Check((isNeedOpen) =>
+				{
+					OnCheckEnd.Invoke();
+					Debug.LogFormat("[CheckerSequence][CheckAndPlay] className Checked {0} isNeedOpen {1}", className, isNeedOpen);
+
+					if (isNeedOpen)
+					{
+						_playedCount++;
+						unit.Play(CheckAndPlay);
+					}
+					else
+					{
+						CheckAndPlay();
+					}
+				});
+			}
+			else
+			{
+				Debug.LogFormat("[UILobbyEnterSequence][CheckAndPlay] All Done.");
+				OnPlayAllDone.Invoke();
+				_index = 0;
+			}
+		}
+	}
+}
---
+++ Assets/ChceckerSequence/Editor/CheckerSequenceTester.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tim.Utility.CheckerSequence
+{
+	public class CheckerSequenceTester : MonoBehaviour
+	{
+		[Test]
+		public void CheckerSequenceChecker()
+		{
+			IChecker ic01 = Substitute.For<IChecker>();
+			ic01.When(x => x.Check(Arg.Any<Action<bool>>()))
+				.Do(x => ((Action<bool>)x[0])(true));
+			ic01.When(x => x.Play(Arg.Any<Action>()))
+				.Do(x => ((Action)x[0]).Invoke());
+
+			IChecker ic02 = Substitute.For<IChecker>();
+			ic02.When(x => x.Check(Arg.Any<Action<bool>>()))
+				.Do(x => ((Action<bool>)x[0])(false));
+			ic02.When(x => x.Play(Arg.Any<Action>()))
+				.Do(x => ((Action)x[0]).Invoke());
+
+			CheckerSequence s = new CheckerSequence();
+			s.Add(ic01);
+			s.Add(ic02);
+
+			s.OnPlayAllDone += () => Assert.AreEqual(1, s.PlayedCount, "PlayedCount Is {0}", s.PlayedCount);
+
+			s.Start();
+
+			ic01.Received().Check((Arg.Any<Action<bool>>()));
+			ic02.Received().Check((Arg.Any<Action<bool>>()));
+
+			ic01.Received().Play(Arg.Any<Action>());
+			ic02.DidNotReceive().Play(Arg.Any<Action>());
+		}
+	}
+}
