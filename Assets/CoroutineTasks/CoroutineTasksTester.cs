64dae453d1138b2626752890cbb6b3f05b18d1fc
timtim <tim12332000@gmail.com>
2018-12-07 02:19:20 +0800

    Add CoroutineTasks & Demo

---
+++ Assets/CoroutineTasks/CoroutineTasksTester.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+namespace Tim.Utility
+{
+	/// <summary>
+	/// Test Evey One Sec Print Log
+	/// </summary>
+	public class CoroutineTasksTester : MonoBehaviour
+	{
+		public void Start()
+		{
+			CoroutineTasks coTasks = new CoroutineTasks();
+			//Dynamic Set Ten IEnumerator
+			for (int i = 0; i < 10; i++)
+			{
+				coTasks.Enqueue(WaitOneSec(i));
+			}
+
+			StartCoroutine(coTasks.Play(() =>
+			{
+				Debug.LogFormat("[CoroutineTasksTester][Start] All IEnumerator Done.");
+			}));
+		}
+
+		private IEnumerator WaitOneSec(int i)
+		{
+			yield return new WaitForSeconds(1f);
+			Debug.LogFormat("[CoroutineTasksTester][WaitOneSec] {0}", i);
+		}
+	}
+}
